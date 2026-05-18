ef26aa5f57cfdae8d24d184ecc981a4b43d16d8e
Devanshukoli <kolidevanshu02@gmail.com>
2026-05-18 15:36:33 +0530

    add styling to header

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,9 @@ function Header() {
 export default function App() {
   return (
     <>
-      <Header />
+      <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
+        <Header />
+      </div>
     </>
   );
 }
