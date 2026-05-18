061999e17119e74d18ebe31ec1d8f1d2ce55dcef
Devanshukoli <kolidevanshu02@gmail.com>
2026-05-18 15:27:09 +0530

    add gray color and sm props to span element

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function Header() {
       <header className="flex items-center justify-between">
         <div className="flex flex-col gap-1">
           <h1 className="text-3xl font-bold">Habit Tracker</h1>
-          <span>1 / 1 done today</span>
+          <span className="text-zinc-400 text-sm">1 / 1 done today</span>
         </div>
         <div></div>
       </header>
