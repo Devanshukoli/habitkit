d23ac4db134c2aeffe5cd885c81da2cafe6f3afa
Devanshukoli <kolidevanshu02@gmail.com>
2026-05-18 15:24:47 +0530

    add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+
+function Header() {
+  return (
+    <>
+      <header className="flex items-center justify-between">
+        <div className="flex flex-col gap-1">
+          <h1>Habit Tracker</h1>
+          <span>1 / 1 done today</span>
+        </div>
+        <div></div>
+      </header>
+    </>
+  );
+}
+
+
 export default function App() {
   return (
     <>
-      <h1>Helo world</h1>
+      <Header />
     </>
   );
 }
