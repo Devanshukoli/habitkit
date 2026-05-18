f0a998aef69c049906264b59266f5b970fc4ee8e
Devanshukoli <kolidevanshu02@gmail.com>
2026-05-18 15:25:58 +0530

    made h1 element bold

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function Header() {
     <>
       <header className="flex items-center justify-between">
         <div className="flex flex-col gap-1">
-          <h1>Habit Tracker</h1>
+          <h1 className="text-3xl font-bold">Habit Tracker</h1>
           <span>1 / 1 done today</span>
         </div>
         <div></div>
