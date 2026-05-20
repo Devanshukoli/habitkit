e6b012c2006259dfa70b6d2e90ff54013b4573d3
Devanshukoli <kolidevanshu02@gmail.com>
2026-05-20 14:30:13 +0530

    update habitform component

--- src/components/HabitForm.tsx
+++ src/components/HabitForm.tsx
@@ -1,7 +1,12 @@
+import Button from "./Button";
+
 const HabitForm = () => {
   return (
     <>
-      <h1>My Habit form</h1>
+      <form className="flex gap-2">
+        <input className="flex-1 rounded-lg bg-zinc-800 px-4 py-2 outline-none focus-visible:ring-2 focus-visible:ring-violet-500" placeholder="New Habit..."/>
+        <Button text="Add habit" />
+      </form>
     </>
   )
 }
