07eca7bd8a5bb17c560c3da808308ed1263b5b21
Devanshukoli <kolidevanshu02@gmail.com>
2026-05-20 14:21:40 +0530

    made header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,5 @@
-import Button from "./components/Button";
 import HabitForm from "./components/HabitForm";
-
-function Header() {
-  return (
-    <>
-      <header className="flex items-center justify-between">
-        <div className="flex flex-col gap-1">
-          <h1 className="text-3xl font-bold">Habit Tracker</h1>
-          <span className="text-zinc-400 text-sm">1 / 1 done today</span>
-        </div>
-        <div className="flex flex-col gap-1">
-          <span className="text-zinc-400 text-sm items-end">May 18 - May 23</span>
-          <div className="flex items-center gap-3">
-            <Button text="Prev" />
-            <Button text="Next" />
-          </div>
-        </div>
-      </header>
-    </>
-  );
-}
+import Header from "./components/Header";
 
 export default function App() {
   return (
---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import Button from "./Button";
+
+const Header = () => {
+  return (
+    <>
+      <header className="flex items-center justify-between">
+        <div className="flex flex-col gap-1">
+          <h1 className="text-3xl font-bold">Habit Tracker</h1>
+          <span className="text-zinc-400 text-sm">1 / 1 done today</span>
+        </div>
+        <div className="flex flex-col gap-1">
+          <span className="text-zinc-400 text-sm items-end">
+            May 18 - May 23
+          </span>
+          <div className="flex items-center gap-3">
+            <Button text="Prev" />
+            <Button text="Next" />
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
