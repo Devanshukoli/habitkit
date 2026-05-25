7840959d507e34c7c20418de08a3793d134c19d8
Devanshukoli <kolidevanshu02@gmail.com>
2026-05-25 10:28:29 +0530

    feat: update header to display today's completed habits dynamically

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,19 @@
 import Button from "./Button";
+import { useHabits } from "../context/useHabits";
+import { isToday } from "date-fns";
 
 const Header = () => {
+
+  const { habits } = useHabits()
+
+  const isDoneToday = habits.filter(h => h.completions.some(c => isToday(c))).length;
+
   return (
     <>
       <header className="flex items-center justify-between">
         <div className="flex flex-col gap-1">
           <h1 className="text-3xl font-bold">Habit Tracker</h1>
-          <span className="text-zinc-400 text-sm">1 / 1 done today</span>
+          <span className="text-zinc-400 text-sm"> {isDoneToday} / {habits.length} done today</span>
         </div>
         <div className="flex flex-col gap-1">
           <span className="text-zinc-400 text-sm items-end">
